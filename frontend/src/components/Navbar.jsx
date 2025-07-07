0ace34e57de1ab78014a5a1793a9f635036cbbe6
ALPER BİLGİN <alper.bilgin@hotmail.com>
2025-07-07 13:23:56 +0300

    React için kütüphaneler eklendi ve temel yapı oluşturuldu

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div>navbar</div>
+      <div>navbar</div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProductPage() {
+  return (
+    <div>
+      <div>A</div>
+      <div>AA</div>
+    </div>
+  );
+}
+
+export default ProductPage;
